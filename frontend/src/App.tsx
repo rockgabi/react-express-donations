3f937c3d8b41874d91eee3f23437f8556c54d8ab
nukkerone <gabi89.m@gmail.com>
2024-01-04 22:54:26 -0300

    Tailwind integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { Outlet } from "react-router-dom";
-
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col">
+      <header className="min-h-24">Header</header>
 
-      <Outlet />
+      <div className="flex-1">
+        <Outlet />
+      </div>
     </div>
   );
 }
