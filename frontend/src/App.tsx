471016875cd396b564d8398c881adb5d9813d032
Gabriel Medina <gabi89.m@gmail.com>
2024-01-05 11:26:29 -0300

    Integrated geist font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,28 +10,30 @@ import {
 
 function App() {
   return (
-    <div className="flex flex-col">
-      <header className="min-h-24 p-2">
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem className="flex gap-2">
-              <NavigationMenuLink asChild>
-                <a className={navigationMenuTriggerStyle()} href="/">
-                  Landing
-                </a>
-              </NavigationMenuLink>
-              <NavigationMenuLink asChild>
-                <a className={navigationMenuTriggerStyle()} href="/">
-                  Gallery
-                </a>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </header>
+    <div className="container">
+      <div className="flex flex-col">
+        <header className="min-h-24 p-2">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem className="flex gap-2">
+                <NavigationMenuLink asChild>
+                  <a className={navigationMenuTriggerStyle()} href="/">
+                    Landing
+                  </a>
+                </NavigationMenuLink>
+                <NavigationMenuLink asChild>
+                  <a className={navigationMenuTriggerStyle()} href="/">
+                    Gallery
+                  </a>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </header>
 
-      <div className="flex-1 p-2">
-        <Outlet />
+        <div className="flex-1 p-2">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -2,5 +2,17 @@
 
 export default function Landing() {
 
-  return <>Landing</>
+  return <div>
+    <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
+      <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1] font-geist">
+        Empower Change: Donate to Inspiring Nonprofit Organizations
+      </h1>
+      <p className="max-w-[750px] text-center text-lg text-muted-foreground sm:text-xl">
+      Transforming Generosity into Impact: Explore and Support Causes That Matter
+      </p>
+      <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
+        
+      </div>
+    </section>
+  </div>
 }
