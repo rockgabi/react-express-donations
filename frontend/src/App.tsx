537ceafc2484a85d7abee818162154bd65e45add
nukkerone <gabi89.m@gmail.com>
2024-01-04 23:43:15 -0300

    Shadcn integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
 import { Outlet } from 'react-router-dom';
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
 
 function App() {
   return (
     <div className="flex flex-col">
-      <header className="min-h-24">Header</header>
+      <header className="min-h-24 p-2">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem className="flex gap-2">
+              <NavigationMenuLink asChild>
+                <a className={navigationMenuTriggerStyle()} href="/">
+                  Landing
+                </a>
+              </NavigationMenuLink>
+              <NavigationMenuLink asChild>
+                <a className={navigationMenuTriggerStyle()} href="/">
+                  Gallery
+                </a>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </header>
 
-      <div className="flex-1">
+      <div className="flex-1 p-2">
         <Outlet />
       </div>
     </div>
