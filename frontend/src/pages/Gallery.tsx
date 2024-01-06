3bad5b223c63fb0d991031e1bc3b52a58473b143
nukkerone <gabi89.m@gmail.com>
2024-01-06 00:42:10 -0300

    Gallery content item

--- frontend/src/pages/Gallery.tsx
+++ frontend/src/pages/Gallery.tsx
@@ -1,13 +1,16 @@
 import GalleryItem from '@/components/gallery-item';
-import { Label } from "@/components/ui/label";
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
-import { Switch } from "@/components/ui/switch";
-import { Separator } from "@radix-ui/react-separator";
+import { Switch } from '@/components/ui/switch';
+import { Cross1Icon } from '@radix-ui/react-icons';
+import { Separator } from '@/components/ui/separator';
+import { Input } from '@/components/ui/input';
 
 export default function Gallery() {
   return (
-    <div className="flex">
-      <section className="flex flex-col gap-4">
+    <div className="flex rounded-[0.5rem] border bg-background shadow-md md:shadow-xl">
+      <section className="flex flex-col gap-4 p-4">
         <div className="flex items-center space-x-2">
           <Switch id="featured" checked />
           <Label htmlFor="featured">Featured</Label>
@@ -21,17 +24,37 @@ export default function Gallery() {
           <Label htmlFor="worldwide">Worldwide</Label>
         </div>
       </section>
-      <Separator orientation="vertical" className="mx-4" />
+      <Separator orientation="vertical" className="h-auto" />
       <ResizablePanelGroup direction="horizontal">
-        <ResizablePanel className="flex flex-row flex-wrap gap-4" defaultSize={75}>
-          <GalleryItem />
-          <GalleryItem />
-          <GalleryItem />
-          <GalleryItem />
+        <ResizablePanel className="" defaultSize={75}>
+          <div className="flex py-2 px-4">
+            <Input type="search" placeholder="Search" className="w-full" />
+          </div>
+          <Separator orientation="horizontal" className="" />
+          <div className="flex flex-row flex-wrap gap-4 p-4">
+            <GalleryItem />
+            <GalleryItem />
+            <GalleryItem />
+            <GalleryItem />
+          </div>
         </ResizablePanel>
-        <ResizableHandle withHandle className="mx-4" />
-        <ResizablePanel className="" defaultSize={25}>
-          Content
+        <ResizableHandle withHandle className="" />
+        <ResizablePanel className="flex flex-col" defaultSize={25}>
+          <div className="flex p-2 items-center">
+            <p className="text-1xl font-semibold leading-none tracking-tight pl-4">Organization ABC</p>
+            <Button variant="ghost" size="icon" className="ml-auto">
+              <Cross1Icon className="h-4 w-4" />
+            </Button>
+          </div>
+          <Separator orientation="horizontal" className="" />
+          <div className="flex flex-col p-4 pt-6 space-y-1.5">
+            <h3 className="text-2xl font-semibold leading-none tracking-tight">Campaign XYZ</h3>
+            <p className="text-sm text-muted-foreground">
+              Our mission is to end global warming, and we need your help. To accomplish this, we need to build a new
+              building.
+              <br /> <br /> Please donate to our campaign and be eligible to win a t-shirt and a sticker.
+            </p>
+          </div>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
