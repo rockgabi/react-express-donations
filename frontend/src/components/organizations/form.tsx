f1a5b37152706d6408a773d397d33567826bfa59
Gabriel Medina <gabi89.m@gmail.com>
2024-01-13 11:05:43 -0300

    Form component for organization model

---
+++ frontend/src/components/organizations/form.tsx
@@ -0,0 +1,93 @@
+import formSchema from './validation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form as UIForm,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '../ui/text-area';
+
+interface Props {
+  onSubmit: (values: z.infer<typeof formSchema>) => void;
+}
+
+export default function Form(props: Props) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+      website: '',
+      tags: [],
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+    props.onSubmit(values);
+  }
+
+  return (
+    <UIForm {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-2xl">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Organization ABC" {...field} />
+              </FormControl>
+              <FormDescription>The name of the organization</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="website"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Website</FormLabel>
+              <FormControl>
+                <Input placeholder="https://organization-abc.com" {...field} />
+              </FormControl>
+              <FormDescription>The website of the organization</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea placeholder="We are an organization dedicated to..." {...field} />
+              </FormControl>
+              <FormDescription>The description of the organization</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </UIForm>
+  );
+}
--- frontend/src/components/organizations/update.tsx
+++ frontend/src/components/organizations/update.tsx
@@ -1,3 +1,17 @@
-export default function Update() {
-  return <>Update</>;
+import Form from './form';
+import formSchema from './validation';
+import * as z from 'zod';
+
+export default function Create() {
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <section>
+      <Form onSubmit={onSubmit} />
+    </section>
+  );
 }
